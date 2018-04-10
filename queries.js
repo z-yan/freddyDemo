89b5ec2cde248f155572d4eb38ee73e93b70524f
Zdravko Yanakiev <zdravko.yanakiev@gmail.com>
2018-04-10 12:27:55 +0200

    Add other query functions

--- queries.js
+++ queries.js
@@ -1,7 +1,6 @@
 const promise = require('bluebird');
 
 const options = {
-    // Initialization Options
     promiseLib: promise
 };
 
@@ -15,7 +14,8 @@ const configuration = {
 };
 const db = pgp(configuration);
 
-// TODO add query functions
+// cosine similarity: requires keyword and number of results
+// TODO different tables
 function getKeywordSimilarity(req, res, next) {
     let keyword = req.query.keyword;
     let results = parseInt(req.query.results);
@@ -34,6 +34,149 @@ function getKeywordSimilarity(req, res, next) {
         });
 }
 
+// kNN queries
+function getKnn(req, res, next) {
+    let title = req.query.title;
+    let index = req.query.index;
+    let results = parseInt(req.query.results);
+    let udf = useIndex(index);
+    let usePv = req.query.use_pv === 'true' ? '_pv' : '';
+
+    db.any('SELECT t.word, t.squaredistance FROM ' + udf + usePv + '($1, $2) AS t ORDER BY t.squaredistance DESC', [title, results])
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved kNNs using ' + index + ' index'
+                });
+        })
+        .catch(function (err) {
+            return next(err);
+        });
+}
+
+function getKnnBatch(req, res, next) {
+    let array = JSON.parse(req.query.array);
+    let k = req.query.k;
+
+    db.any('SELECT * FROM k_nearest_neighbour_ivfadc_batch($1, $2)', [array, k])
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved batch kNN'
+                });
+        })
+        .catch(function (err) {
+            return next(err);
+        });
+}
+
+function getKnnIn(req, res, next) {
+    let title = req.query.title;
+    let results = parseInt(req.query.results);
+    let outputSet = JSON.parse(req.query.output_set);
+    let usePq = useIndex(req.query.use_pq);
+
+    db.any('SELECT * FROM knn_in' + usePq + '($1, $2, $3)', [title, results, outputSet])
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved kNN with specific output set'
+                });
+        })
+        .catch(function (err) {
+            return next(err);
+        });
+}
+
+function getAnalogy(req, res, next) {
+    let arg1 = req.query.arg1;
+    let arg2 = req.query.arg2;
+    let arg3 = req.query.arg3;
+    let index = req.query.index;
+    let udf = useIndex(index);
+
+    db.any('SELECT * FROM analogy_3cosadd' + udf + '($1, $2, $3)', [arg1, arg2, arg3])
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved analogy'
+                });
+        })
+        .catch(function (err) {
+            return next(err);
+        });
+}
+
+function getAnalogyIn(req, res, next) {
+    let arg1 = req.query.arg1;
+    let arg2 = req.query.arg2;
+    let arg3 = req.query.arg3;
+    let outputSet = JSON.parse(req.query.output_set);
+    let usePq = useIndex(req.query.use_pq);
+
+    db.any('SELECT result FROM analogy_3cosadd_in' + usePq + '($1, $2, $3, $4::varchar(100)[])', [arg1, arg2, arg3, outputSet])
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved analogy with specific output set'
+                });
+        })
+        .catch(function (err) {
+            return next(err);
+        });
+}
+
+function getGrouping(req, res, next) {
+    let tokens = JSON.parse(req.query.tokens);
+    let groupTokens = JSON.parse(req.query.group_tokens);
+    let usePq = useIndex(req.query.use_pq);
+
+    db.any('SELECT token, grouptoken FROM grouping_func' + usePq + '($1, $2)', [tokens, groupTokens])
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved token grouping'
+                });
+        })
+        .catch(function (err) {
+            return next(err);
+        });
+}
+
+function useIndex(index) {
+    let udf;
+
+    if (index === 'ivfadc') {
+        udf = '_ivfadc';
+    }
+    else if (index === 'pq') {
+        udf = '_pq';
+    }
+    else {
+        udf = '';
+    }
+
+    return udf;
+}
+
 module.exports = {
-    getKeywordSimilarity: getKeywordSimilarity
+    getKeywordSimilarity: getKeywordSimilarity,
+    getKnn: getKnn,
+    getKnnBatch: getKnnBatch,
+    getKnnIn: getKnnIn,
+    getAnalogy: getAnalogy,
+    getAnalogyIn: getAnalogyIn,
+    getGrouping: getGrouping
 };
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,13 @@ const router = express.Router();
 
 const db = require('../queries');
 
+// RESTful API routes
 router.get('/api/similarity', db.getKeywordSimilarity);
+router.get('/api/knn', db.getKnn);
+router.get('/api/knn_batch', db.getKnnBatch);
+router.get('/api/knn_in', db.getKnnIn);
+router.get('/api/analogy', db.getAnalogy);
+router.get('/api/analogy_in', db.getAnalogyIn);
+router.get('/api/grouping', db.getGrouping);
 
 module.exports = router;
