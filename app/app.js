2b668a499f46bb659c35a0be8230a572c147c99b
Zdravko Yanakiev <zdravko.yanakiev@gmail.com>
2018-04-17 15:14:04 +0200

    Begin frontend template

--- app/app.js
+++ app/app.js
@@ -3,17 +3,23 @@ const freddyDemo = angular.module('freddyDemo', [
     'ui.bootstrap'
 ]);
 
+// Configure routes
 freddyDemo.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
     $routeProvider
         .when("/", {
-            templateUrl: '/app/test.html',
-            controller: 'prototypeController'
+            templateUrl: '/app/main.html',
         })
         .otherwise({redirectTo: '/'});
 
     $locationProvider.html5Mode(true);
 }]);
 
+freddyDemo.controller('MainController', function ($scope) {
+    $scope.isSettingsCollapsed = true;
+    $scope.isQueriesCollapsed = true;
+});
+
+// Test controller
 freddyDemo.controller('prototypeController', function ($scope, $http) {
     $scope.getRequest = function () {
         $http.get('http://141.76.47.127:3000/api/similarity?keyword=' + $scope.keyword + '&results=' + $scope.results)
