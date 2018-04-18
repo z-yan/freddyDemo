16102cdb74dc01d1799451bb48df04814b2f3e7d
Zdravko Yanakiev <zdravko.yanakiev@gmail.com>
2018-04-18 14:48:07 +0200

    Show list of tables in DB

--- app/app.js
+++ app/app.js
@@ -4,14 +4,28 @@ const freddyDemo = angular.module('freddyDemo', [
     'hljs'
 ]);
 
-freddyDemo.controller('MainController', function ($scope) {
+freddyDemo.controller('MainController', function ($scope, $http) {
     $scope.isSettingsCollapsed = true;
     $scope.isQueriesCollapsed = true;
+
+    $scope.dbOptions = ['IMDb', 'Discogs'];
+
+    $scope.selectedDb = $scope.dbOptions[0];
+
+    $scope.updateTableList = function (dbName) {
+        $scope.selectedDb = dbName;
+
+        $http.get('http://141.76.47.127:3000/api/tables?db=' + dbName.toLowerCase())
+            .then(function successCallback(response) {
+                $scope.tables = response.data.data;
+            }, function errorCallback(response) {
+                console.log("Unable to perform get request");
+            });
+    };
+
+    $scope.updateTableList('imdb');
 });
 
-freddyDemo.controller('DbSchemaController', function ($scope) {
-
-});
 
 // Test controller
 freddyDemo.controller('PrototypeController', function ($scope, $http) {
