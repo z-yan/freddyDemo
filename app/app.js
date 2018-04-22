2d10c0ce3f11f79d3cde5e6e8cff6de95d778cf8
Zdravko Yanakiev <zdravko.yanakiev@gmail.com>
2018-04-22 13:05:21 +0200

    Add example queries file, small fixes

--- app/app.js
+++ app/app.js
@@ -15,15 +15,25 @@ freddyDemo.controller('MainController', function ($scope, $http) {
     $scope.updateTableList = function (dbName) {
         $scope.selectedDb = dbName;
 
-        $http.get('http://141.76.47.127:3000/api/tables?db=' + dbName.toLowerCase())
+        $http.get('/api/tables?db=' + dbName.toLowerCase())
             .then(function successCallback(response) {
                 $scope.tables = response.data.data;
             }, function errorCallback(response) {
-                console.log("Unable to perform get request");
+                console.log("Unable to fetch tables in schema.");
+            });
+    };
+
+    $scope.getQueryList = function () {
+        $http.get('example_queries.json')
+            .then(function successCallback(data) {
+                console.log(data);
+            }, function errorCallback() {
+                console.log("Unable to load query JSON file.");
             });
     };
 
     $scope.updateTableList($scope.selectedDb);
+    $scope.getQueryList();
 });
 
 
